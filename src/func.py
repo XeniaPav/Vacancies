b87b19deac011116d062edae4be9e8d8ab8fe201
Pavlukhina Ksenia <kseny100297@yandex.ru>
2024-05-24 16:28:13 +0500

    add main program code

---
+++ src/func.py
@@ -0,0 +1,27 @@
+from classes import HeadHunterAPI
+
+def user_interaction():
+    """
+    Функция для взаимодействия с пользователем
+    :return: словарь найденных вакансий по запросу пользователя
+    """
+    quantity = int(input('Введите количество вакансий для поиска:'))
+    keyword = str(input('Введите ключевое слово:'))
+    hh_api = HeadHunterAPI()
+    vacancies = hh_api.get_vacancies(keyword, quantity)
+    print(f'Найдено {len(vacancies)} вакансий')
+    return vacancies
+
+def choise_command():
+    """
+    Функция для выбора действий пользователя
+    :return: команда пользователя
+    """
+    command = int(input('Выберите действия:\n'
+                        '1 - найти другие вакансии\n'
+                        '2 - вывести найденные вакансии в терминале\n'
+                        '3 - отсортировать по зарплате\n'
+                        '4 - удалить вакансию из файла\n'
+                        '5 - посмотреть сохранённые вакансии\n'
+                        '6 - выйти\n'))
+    return command
---
+++ tests/test_func.py
@@ -0,0 +1,1 @@
+from src.func import user_interaction
